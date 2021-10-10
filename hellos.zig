a3b198302b92fb333abb3bac79d5ec1b2381da55
Ali Chraghi <63465728+AliChraghi@users.noreply.github.com>
2021-10-10 10:19:33 +0330

    work with the latest version

--- hellos.zig
+++ hellos.zig
@@ -1,4 +1,4 @@
-const builtin = @import("builtin");
+const builtin = @import("std").builtin;
 
 const MultiBoot = packed struct {
     magic: i32,
@@ -26,7 +26,7 @@ export fn _start() callconv(.Naked) noreturn {
     while (true) {}
 }
 
-pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
+pub fn panic(msg: []const u8, _: ?*builtin.StackTrace) noreturn {
     @setCold(true);
     terminal.write("KERNEL PANIC: ");
     terminal.write(msg);
